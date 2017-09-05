8e4571a79ff219e46b04743544fc4910141b330b
Frances Morales Velilla <frances0688@gmail.com>
2017-09-05 09:23:44 +0200

    missing last iteration

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -3,17 +3,52 @@ class Elevator {
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.direction  = "up";
+    this.waitingList= [];
+    this.passengers = [];
+  }
+  
+  start() { 
+    this.interval = setInterval(()=>{
+      this.update();
+    }, 1000);
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  stop() {
+    clearInterval(this.interval);
+   }
+
+  update() { 
+    console.log(this.log());
+  }
+
+  _passengersEnter() { 
+
+  }
+
+  _passengersLeave() {
+
+   }
+
+  floorUp() {
+    if (this.floor < this.MAXFLOOR){
+    return this.floor += 1;
+    }
+   }
+
+  floorDown() { 
+    if (this.floor > 0){
+      return this.floor -= 1;
+    }
+  }
+
+  call(person) { 
+    this.requests.push(person);
+  }
+
+  log() { 
+    return `Direction: ${this.direction} | Floor:  ${this.floor}`
+  }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,10 @@
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+
+const otherElevator = new Elevator();
+
+otherElevator.start();
+otherElevator.floorUp();
+otherElevator.update();
+otherElevator.floorDown();
+otherElevator.update();
